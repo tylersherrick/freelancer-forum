e0471a4611a3616f4b5ebb1ec5c66310466223ad
Tyler Sherrick <tyler.sherrick@gmail.com>
2026-04-09 17:51:39 -0500

    have the assignment pretty much finished need to finish up the formatting.

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+const appData = document.getElementById("app");
+
+const getName = (NAMES) => {
+    return NAMES[Math.floor(Math.random() *  NAMES.length)];
+}
+
+const getOccupation = (OCCUPATIONS) => {
+    return OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+}
+
+const getPrice = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const people = Array.from({length: NUM_FREELANCERS}, () => ({
+    name: getName(NAMES),
+    occupation: getOccupation(OCCUPATIONS),
+    price: getPrice(PRICE_RANGE.min, PRICE_RANGE.max)
+}));
+
+appData.innerHTML = people.map((person, index) => `
+        <div>
+            <p>${index + 1}: ${person.name} - ${person.occupation} - $${person.price}</p>
+        </div>
+    `).join("");
